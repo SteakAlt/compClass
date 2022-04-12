6aea15ef00c716ca8dfaccd091e2a031ea6ae1e5
SteakAlt <102618624+SteakAlt@users.noreply.github.com>
2022-04-12 21:17:44 +0400

    Update StudentInfo.py

--- Jaydeep/StudentInfo.py
+++ Jaydeep/StudentInfo.py
@@ -5,9 +5,20 @@ Write a program to store students info like admission number, role number, name 
 keys. Display info on the basis of admission number obtained as user input.
 '''
 
-d = {1 : ['01', 'Jay', '90'], 2 : ['02', 'Jaeden', '95']}
+n = int(input("Enter number of students: "))
+d = {}
+for i in range(n):
+    adm = int(input("Enter admission number: "))
+    role = input("Enter role number: ")
+    name = input("Enter name: ")
+    marks = input("Enter marks: ")
+    d[adm] = [role, name, marks]
 
-adm = int(input("Enter admission number: "))
-l = d[adm]
 
-print("Role number: " + l[0] + "\nName: " + l[1] + "\nMarks: " + l[2])
+
+print()
+
+adm = int(input("Enter admission number of student: "))
+
+print("Role number: " + d[adm][0] + "\nName: " + d[adm][1] + "\nMarks: " + d[adm][2])
+    
